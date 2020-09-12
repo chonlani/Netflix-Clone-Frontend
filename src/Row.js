579577a7562064a12b33041e870708d2321fadfe
chonlani <53692121+chonlani@users.noreply.github.com>
2020-09-12 20:10:22 +0700

    banner

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from "./axios"
 import requests from './requests';
+import './Row.css';
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
     const [ movies, setMovies] = useState([]);
 
     //A snippet of code which runs based on a specific condition
@@ -28,7 +29,11 @@ function Row({ title, fetchUrl }) {
               {/* several row__poster(s) */}
 
               {movies.map(movie => (
-                  <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />        
+                  <img 
+                  key={movie.id}
+                  className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                  src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                  alt={movie.name} />        
                   ))}
             </div>
 
